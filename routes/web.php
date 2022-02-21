d1bc5df0cc4ad6f8fd439890fb70b5449604db25
Nirhor3029 <sazzad3029@gmail.com>
2022-02-21 09:39:37 +0600

    websockets connection successfully established

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/trigger/{data}', function ($data) {
+    echo "<p>You have sent $data</p>";
+    event(new App\Events\GetRequestEvent($data));
+});
